ae33ab50f8db18496714274c303e2f8cb4be75de
Isaac Sanchez <isaacsanchezmonge@gmail.com>
2026-07-25 15:52:39 -0600

    Expose regression failures in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -62,7 +62,9 @@ export default defineConfig({
   fullyParallel: true,
   retries: process.env.CI ? 2 : 0,
   workers: 1,
-  reporter: process.env.CI ? [["list"], ["html", { open: "never" }]] : "list",
+  reporter: process.env.CI
+    ? [["github"], ["list"], ["html", { open: "never" }]]
+    : "list",
   use: {
     baseURL,
     trace: "on-first-retry",
